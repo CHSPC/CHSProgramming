9e03851be91d6ac9949bc860c82494ce543d8738
atf1999 <torth212@gmail.com>
2017-01-02 22:55:38 -0500

    Comments

--- BinarySearch/src/BinarySearch.java
+++ BinarySearch/src/BinarySearch.java
@@ -1,19 +1,28 @@
 import java.util.Arrays;
 public class BinarySearch{
 	public static void main(String[] args){
+		//Array to search in
 		int[] A = {1,2,3,4,5};
+		//Search happens
 		System.out.println(search(A, 6));
 
 	}
 
 	public static int search(int[] A, int T){
+		//L - minimum index, R - maximum index
 		int L = 0, R = A.length - 1;
+		//Search condition
 		while(L <= R){
+			//m - middle index
 			int m = (int)Math.floor((L+R)/2);
+			//if the middle element is less than the target, then chop off everything to the left
 			if(A[m] < T) L = m + 1;
+			//if the middle element is bigger, then chop off everthing to the right
 			if(A[m] > T) R = m - 1;
+			//Hery u found the target, Good job
 			if(A[m] == T) return m; 
 		}
+		//Welp you failed to find the target. GG
 		return -1;
 
 	}
